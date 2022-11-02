c81ca11de3093479e7e5404c49e32d51657e4c52
Adrian Ściepura <adriansciepura@outlook.com>
2022-11-02 22:36:15 +0100

    Added mobile responsiveness

--- activity-expander.js
+++ activity-expander.js
@@ -1,15 +1,36 @@
-function load()
+var page = window.screen.width;
+var start_height = "";
+window.load = load;
+window.onresize = resize;
+
+function size()
 {
-    const elements = document.getElementsByClassName("activity");
-    for(var i = 0; i < elements.length; i++)
+    if(page > 500)
     {
-        elements[i].style.height = "500px";
+        start_height = "500px";
     }
+    else
+    {
+        start_height = "200px";
+    }
+
+    const elements = document.getElementsByClassName("activity");
+    for(var i of elements)
+    {
+        i.style.height = start_height;
+    }
+}
+
+function resize()
+{
+    page = window.screen.width;
+    size();
+
 }
 
 function expand(element){
     const height = element.style.height;
-    if(height === "500px")
+    if(height == start_height)
     {
         element.style.height = "auto";
         element.style.opacity = "1";
@@ -17,8 +38,13 @@ function expand(element){
     }
     else
     {
-        element.style.height = "500px";
+        element.style.height = start_height;
         element.children[1].style.display = "none";
         element.style.opacity = "0.8";
     }
+}
+
+function load()
+{
+    size();
 }
