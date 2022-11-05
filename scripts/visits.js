18a73326ef6ab4aebd07d396cabc58058f2e163e
Adrian Ściepura <adriansciepura@outlook.com>
2022-11-05 16:41:02 +0100

    Changed visits script
    Implementation moved from JavaScript to TypeScript

--- scripts/visits.js
+++ scripts/visits.js
@@ -1,15 +1,12 @@
 window.onload = load;
-
-function load()
-{
-    var visits = localStorage.getItem('visits');
-    if(visits==null) visits = 0;
-    
-    var wrapper = document.getElementsByClassName('wrapper');
-    const new_element = document.createElement('p');
-    new_element.classList.add('regular-text')
-    const new_element_text = document.createTextNode('Liczba odwiedzin strony: ' + visits);
+function load() {
+    var storage = localStorage.getItem('visits');
+    var visits = storage == null ? 0 : parseInt(storage);
+    var wrapper = document.querySelector('.wrapper');
+    var new_element = document.createElement('p');
+    var new_element_text = document.createTextNode('Liczba odwiedzin strony: ' + visits);
+    new_element.classList.add('regular-text');
     new_element.appendChild(new_element_text);
-    wrapper[0].appendChild(new_element);
-    localStorage.setItem('visits', parseInt(visits)+1);
+    wrapper === null || wrapper === void 0 ? void 0 : wrapper.appendChild(new_element);
+    localStorage.setItem('visits', (visits + 1).toString());
 }
---
+++ scripts/visits.ts
@@ -0,0 +1,13 @@
+window.onload = load;
+
+function load(): void {  
+    var storage = localStorage.getItem('visits');
+    var visits: number = storage == null ? 0 : parseInt(storage); 
+    var wrapper = document.querySelector('.wrapper');
+    const new_element = document.createElement('p');
+    const new_element_text = document.createTextNode('Liczba odwiedzin strony: ' + visits);
+    new_element.classList.add('regular-text');
+    new_element.appendChild(new_element_text);
+    wrapper?.appendChild(new_element);
+    localStorage.setItem('visits', (visits+1).toString());
+}
